128eb3b7b11025486eb0137bcd92c511660dede4
Tyler Lynch <tylerlynch01@gmail.com>
2019-09-22 16:06:18 -0400

    first commit

---
+++ src/MainMenu.java
@@ -0,0 +1,5 @@
+public class MainMenu {
+    public MainMenu(){
+
+    }
+}
---
+++ src/Start.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Start extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Coffee Shop");
+        primaryStage.show();
+    }
+
+}
