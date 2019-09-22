ad7b5b0bc791476918cb9024138a9c778ee01604
Tyler Lynch <tylerlynch01@gmail.com>
2019-09-22 16:57:24 -0400

    create main menu

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,5 +1,32 @@
-public class MainMenu {
-    public MainMenu(){
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 
+public class MainMenu {
+
+    private GridPane _grid;
+    private Scene _scene;
+
+    public MainMenu(){
+        _grid = new GridPane();
+        _scene = new Scene(_grid, 500, 400);
     }
+
+    public void SetMainMenuGrid(){
+        _grid.setAlignment(Pos.CENTER);
+        _grid.setHgap(10);
+        _grid.setVgap(10);
+        _grid.setPadding(new Insets(25, 25, 25, 25));
+    }
+
+    public Scene GetMainMenuScene(){
+        return this._scene;
+    }
+
+    public void AddToGrid(Node obj, int width, int height ){
+        _grid.add(obj, width, height);
+    }
+
 }
--- src/Start.java
+++ src/Start.java
@@ -1,4 +1,9 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Start extends Application {
@@ -11,6 +16,35 @@ public class Start extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Coffee Shop");
         primaryStage.show();
+
+        //load menu
+        MainMenu mainMenu = new MainMenu();
+
+        mainMenu.SetMainMenuGrid();
+        Scene scene = mainMenu.GetMainMenuScene();
+
+        primaryStage.setScene(scene);
+
+        //add to menu
+        Label coffeeLabel = new Label("Select Beverage: ");
+        mainMenu.AddToGrid(coffeeLabel, 0,1);
+
+        ComboBox coffeeComboBox = new ComboBox<>();
+        coffeeComboBox.getItems().add("Hot Coffee");
+        coffeeComboBox.getItems().add("Iced Coffee");
+        coffeeComboBox.getItems().add("Hot Tea");
+        mainMenu.AddToGrid(coffeeComboBox, 1,1);
+
+
+        Label lbl = new Label("How many cups of coffee would you like?");
+        mainMenu.AddToGrid(lbl, 0,2);
+
+        TextField tf = new TextField();
+        mainMenu.AddToGrid(tf,1,2);
+
+        Button orderButton = new Button("Order");
+        mainMenu.AddToGrid(orderButton, 0,3);
+
     }
 
 }
